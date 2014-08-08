1db90a32dbd19149f5018d85c7b7fe36e334c593
Anthony Wilkins <accelleon@live.com>
2014-08-08 06:08:57 -0400

    Commenting

--- tokenizer.lua
+++ tokenizer.lua
@@ -1,5 +1,8 @@
+-- Tokenizer
+-- Reads the file and puts it into a form
+-- we can understand
 
-
+-- Gets the next file in the chain
 function ASM:nextFile()
 	-- Save fname and line in case of EOF
 	local tmp = self.Source[1]
@@ -9,6 +12,7 @@ function ASM:nextFile()
 	end
 end
 
+-- Get the next character from the file
 function ASM:getChar()
 	local char = string.sub(self.Source[1].Src,1,1)
 	if char == "" then
@@ -18,6 +22,8 @@ function ASM:getChar()
 	return char
 end
 
+-- Get the next character from the file
+-- Keep track of current location in file
 function ASM:nextChar()
 	local source = self.Source[1]
 	if source.Src == "" then
@@ -35,6 +41,7 @@ function ASM:nextChar()
 	end
 end
 
+-- Skip whitespace, retains 1 newline character
 function ASM:skipSpace()
 	local nl = false
 	local ch = self:getChar()
@@ -58,19 +65,26 @@ function ASM:skipSpace()
 	end
 end
 
+-- Returns the current position
 function ASM:GetFileLine()
 	return self.Source[1].Fname, self.Source[1].Line
 end
 
+-- This function actually converts into tokens
 function ASM:doToken()
+	-- Faster access
 	local TOKEN = self.TOKEN
+	-- Skip whitespace
 	self:skipSpace()
 	
+	-- Save our current position
 	local tokPos = { Line = self.Source[1].Line,
 					 Col = self.Source[1].Col,
 					 File = self.Source[1].Fname }
 	
+	-- Have we hit end of file?
 	if self:getChar() == "" then
+		-- Yes add EOF token
 		table.insert(self.Tokens, {
 			Type = TOKEN.EOF,
 			Data = nil,
@@ -78,25 +92,33 @@ function ASM:doToken()
 		return false
 	end
 	
+	-- Check for preprocessor macros
+	-- i.e. a % sign on the first character of the line
 	if (tokPos.Col == 1) and (self:getChar() == "%") then
 		local macroLine = ""
+		-- The entire line is a macro
 		while (self:getChar() ~= "") and (self:getChar() ~= "\n") do
 			macroLine = macroLine .. self:getChar()
 			self:nextChar()
 		end
 		
+		-- Execute the preprocessor
 		self:PreprocessLine(macroLine, tokPos)
 		return true
 	end
 	
+	-- Is it a string?
 	if (self:getChar() == "'") or (self:getChar() == "\"") then
+		-- Yes save what type ("str" is different from 'c')
 		local stringType = self:getChar()
-		self:nextChar()
+		self:nextChar() -- start of string
 		
 		local str = ""
+		-- Until we hit the next quote continue or EOF
 		while (self.Source.Src ~= "") and (self:getChar() ~= "'")
 			and (self:getChar() ~= "\"") do
 			
+			-- Special escape characters
 			if self:getChar() == "\\" then
 				self:nextChar()
 				local ch = self:getChar()
